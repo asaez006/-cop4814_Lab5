a1055f80efc8582601a96082b558a17543a49f3e
Andre Saez <33471426+asaez006@users.noreply.github.com>
2017-11-07 20:36:09 -0500

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    public class Game
+    {
+        public string Team1 { get; set; }
+        public string Team1Scores { get; set; }
+        public string Team2 { get; set; }
+        public string Team2Scores { get; set; }
+        public Game() { }
+
+        public Game(string Team1, string Team1Scores, string Team2, string Team2Scores)
+        {
+            this.Team1 = Team1;
+            this.Team1Scores = Team1Scores;
+            this.Team2 = Team2;
+            this.Team2Scores = Team2Scores;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} ({1}) - {2} ({3})", Team1, Team1Scores, Team2, Team2Scores);
+        }
+        
+    }
+
+}
---
+++ GameFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab_2
+{
+    public class GameFactory
+    {
+        StreamWriter sw;
+        XmlSerializer serial;
+        List<Game> gameList;
+
+        const string FilePath = @"..\..\games.xml";
+        public void CreateGameList()
+        {
+            gameList = new List<Game>();
+            Game G = new Game("Hornets", "50", "Bulldogs", "73");
+            gameList.Add(G);
+            G = new Game("Heat", "100", "Wizards", "94");
+            gameList.Add(G);
+            G = new Game("Warriors", "122", "Cavaliers", "121");
+            gameList.Add(G);
+            G = new Game("Bucks", "88", "Celtics", "94");
+            gameList.Add(G);
+            G = new Game("Rockets", "98", "Thunder", "101");
+            gameList.Add(G);
+            G = new Game("Spurs", "109", "Wizards", "107");
+            gameList.Add(G);
+        }
+        public Boolean SerializeGameList()
+        { 
+            serial = new XmlSerializer(gameList.GetType());
+            sw = new StreamWriter(FilePath);
+            serial.Serialize(sw, gameList);
+            sw.Close();
+            return true;
+        }
+
+        public List<Game> DeserializeGameList(out string resultMessage)
+        {
+            try
+            { 
+                gameList = new List<Game>();
+                StreamReader sr = new StreamReader(FilePath);
+                serial = new XmlSerializer(gameList.GetType());
+                gameList = (List<Game>)serial.Deserialize(sr);
+                sr.Close();
+                resultMessage = "It Worked!";
+                return gameList;
+            }
+            catch(Exception ex)
+            {
+                resultMessage = "Exception: " + ex.Message;
+                return null;
+            }
+        }
+    }
+}
